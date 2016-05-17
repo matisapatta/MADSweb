c7af25495d973a8979b610a899eda9dc60406e37
matisapatta <mati.sapatta@gmail.com>
2016-05-17 13:25:46 -0300

    .

---
+++ assets/js/MADSapp.js
@@ -0,0 +1,16 @@
+/**
+ * Created by mati on 5/17/16.
+ */
+angular.module('MADSapp', ['duScroll']).
+    controller('MyCtrl', function($scope, $document){
+        $scope.toTheTop = function() {
+            $document.scrollTopAnimated(0, 5000).then(function() {
+                console && console.log('You just scrolled to the top!');
+            });
+        }
+        var section3 = angular.element(document.getElementById('section-3'));
+        $scope.toSection3 = function() {
+            $document.scrollToElementAnimated(section3);
+        }
+    }
+).value('duScrollOffset', 30);
